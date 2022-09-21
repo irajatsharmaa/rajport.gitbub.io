58720170da9e3eab74abd35702af949dba423dfb
irajatsharmaa <111880786+irajatsharmaa@users.noreply.github.com>
2022-09-21 23:04:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -45,10 +45,10 @@ const tabs=document.querySelectorAll('[data-target]'),
 tabContents=document.querySelectorAll('[data-content]');
 tabs.forEach(tab =>{
     tab.addEventListener('click',()=>{
-        const target=document.querySelector(tab.dataset.target)
+        const target=document.querySelector(tab.dataset.target);
 
         tabContents.forEach(tabContent =>{
-            tabContent.classList.remove('qualification__active')
+            tabContent.classList.remove('qualification__active');
         })
         target.classList.add('qualification__active')
         tabs.forEach(tab =>{
